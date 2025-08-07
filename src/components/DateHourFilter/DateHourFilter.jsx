cb7943fae0832e74a637c3c9b87c29934d8a6471
Abhijith M R <abhijithmr581@gmail.com>
2025-08-07 17:27:34 +0530

    minor changes

--- src/components/DateHourFilter/DateHourFilter.jsx
+++ src/components/DateHourFilter/DateHourFilter.jsx
@@ -1,22 +1,34 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setDateFilter, setHourFilter } from '../../store/slices/audioSegmentsSlice';
+import { useSearchParams } from 'react-router-dom';
 
 const DateHourFilter = () => {
   const dispatch = useDispatch();
   const { date, hour } = useSelector((state) => state.audioSegments.filters);
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [selectedDate, setSelectedDate] = useState(date);
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const handleDateChange = (e) => {
     const newDate = e.target.value.replace(/-/g, '');
     setSelectedDate(newDate);
     dispatch(setDateFilter(newDate));
     setShowDatePicker(false);
+    // Update URL search params
+    setSearchParams({
+      date: newDate,
+      hour: searchParams.get('hour') || '0',
+    });
   };
 
   const handleHourChange = (newHour) => {
     dispatch(setHourFilter(newHour.toString()));
+    // Update URL search params
+    setSearchParams({
+      date: searchParams.get('date') || date || new Date().toISOString().split('T')[0].replace(/-/g, ''),
+      hour: newHour.toString(),
+    });
   };
 
   const formatDateForDisplay = (dateStr) => {
