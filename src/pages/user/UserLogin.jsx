8e0d88ad455df60481d527fa9dcb608606162693
Rony George <ronygeorgengit@gmail.com>
2025-09-20 00:31:10 +0530

    removed divider

--- src/pages/user/UserLogin.jsx
+++ src/pages/user/UserLogin.jsx
@@ -252,7 +252,7 @@ const UserLogin = () => {
               </button>
 
               {/* Divider */}
-              <div className="mt-6">
+              {/* <div className="mt-6">
                 <div className="relative">
                   <div className="absolute inset-0 flex items-center">
                     <div className="w-full border-t border-gray-300" />
@@ -261,7 +261,7 @@ const UserLogin = () => {
                     <span className="px-2 bg-white text-gray-500">or</span>
                   </div>
                 </div>
-              </div>
+              </div> */}
 
               {/* Create Account Link */}
               {/* <div className="text-center">
